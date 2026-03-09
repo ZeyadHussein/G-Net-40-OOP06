02d4c2afbae303bffd87ecc60add4cc6e1b3a75d
Zeyad Hussein <zeyad02_hussein@hotmail.com>
2026-03-09 15:58:28 +0200

    part 1 qouestion 1

--- Assigment OOP 6/Program.cs
+++ Assigment OOP 6/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 1: Theoretical Questions
+          #region Qouestion 1
+            //        /*
+            //        Q1: What is abstraction in OOP? How is it different from encapsulation?
+            //        Give a real-world example (not from the session) that shows the difference.
+            //        */
+
+            //        Answer:
+
+            //            Abstraction is the concept of hiding complex implementation details and showing
+            //            only the essential features of an object.It focuses on what an object does
+            //instead of how it does it.
+
+            //Encapsulation is the concept of wrapping data and methods together inside a
+            //class and restricting direct access to some of the object's data using
+            //access modifiers like private or protected.
+
+            //Difference:
+            //- Abstraction hides complexity.
+            //- Encapsulation protects data.
+
+            //Real-world example:
+            //A coffee machine.
+
+            //Abstraction:
+            //A user presses the "Make Coffee" button without knowing how the machine heats
+            //water or mixes coffee.
+
+            //Encapsulation:
+            //The internal components like the heater and pump are hidden inside the machine
+            //and cannot be accessed directly by the user.
+            //*/
+            #endregion
+            #endregion
         }
     }
 }
