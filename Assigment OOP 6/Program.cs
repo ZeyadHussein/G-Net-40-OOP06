b56cd08726e9681258ec13e48ee251b295971476
Zeyad Hussein <zeyad02_hussein@hotmail.com>
2026-03-09 16:00:00 +0200

    part 1 qouestion 2

--- Assigment OOP 6/Program.cs
+++ Assigment OOP 6/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace Assigment_OOP_6
+﻿using System.ComponentModel;
+using System.Runtime.Intrinsics.X86;
+
+namespace Assigment_OOP_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             #region Part 1: Theoretical Questions
-          #region Qouestion 1
+            #region Qouestion 1
             //        /*
             //        Q1: What is abstraction in OOP? How is it different from encapsulation?
             //        Give a real-world example (not from the session) that shows the difference.
@@ -36,6 +39,44 @@
             //The internal components like the heater and pump are hidden inside the machine
             //and cannot be accessed directly by the user.
             //*/
+            #endregion
+            #region Q2
+            /*
+            Q2: What is the difference between an abstract class and an interface?
+            Give at least four differences. When would you choose one over the other?
+            */
+
+            //        Answer:
+
+            // 1.Implementation
+            //Abstract Class:Can contain both implemented methods and abstract methods.
+
+            //Interface:
+            //Contains only method declarations(no implementation).
+
+            //2. Constructors
+            //Abstract Class:Can have constructors.
+
+            //Interface:Cannot have constructors.
+
+            //3. Fields
+            //Abstract Class: Can have fields and properties.
+
+            //Interface: Cannot contain fields.
+
+            //4. Inheritance
+            //Abstract Class:A class can inherit from only one abstract class.
+
+            //Interface:A class can implement multiple interfaces.
+
+            //When to use:
+
+            //Use an Abstract Class when:
+            // Classes share common code. You want to provide base functionality
+
+            //Use an Interface when:
+            //You want to define a contract. Multiple unrelated classes must follow the same behavior
+            //
             #endregion
             #endregion
         }
